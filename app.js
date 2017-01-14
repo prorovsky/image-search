3fd792a6ecca0a660d8f8d42ec4b2f90bc81b689
Pavel Prorovsky <prorovsky@gmail.com>
2017-01-14 19:05:18 +0300

    add db and history

--- app.js
+++ app.js
@@ -1,37 +1,80 @@
 var express = require('express'),
     app = express(),
     request = require('request'),
+    config = require('./config'),
     mongoose = require('mongoose');
 
+// db
+var dburl = process.env.DATABASEURL || "mongodb://localhost/fcc-backend";
+mongoose.connect(dburl, function(err, db){
+    if(err){
+        console.error(err);
+    } else {
+        console.log('Successfully connected to DB');
+    }
+});
+var searchSchema = new mongoose.Schema({
+   search: String,
+   time: String 
+});
+var Search = mongoose.model('Search', searchSchema);
+
+// routes
 app.get('/:hello', function(req, res){
     res.send('hello');
 });
 
 app.get('/api/imagesearch/:query', function(req, res){
+    createHistory(req);
     goGoogle(req, res);
 });
 
 app.get('/api/latest/imagesearch/', function(req, res){
-
-    // request db for user's history
-    res.send('history');
+    Search.find({}, '-_id -__v', {sort: {time: -1}}, function(err, data){
+        res.json(data);
+    }).limit(10);
 });
 
 app.listen(process.env.PORT || 3000, function(){
     console.log('Server started');
 });
 
+// helpers
 function goGoogle(req, res){
-    
-        var options = {
-            url: `${mainUrl}${userSearch}${SEID}${image}${offset}${APIKEY}` 
-        };
+    var userSearch = req.params.query,
+        offset = req.query.offset || 1;
+    if(!+req.query.offset || req.query.offset > 90){
+        offset = 1;
+    } 
+    var options = {
+        url: `${config.mainUrl}${userSearch}${config.SEID}${config.image}&start=${offset}${config.APIKEY}` 
+    };
 
     request(options, getData);
     function getData(err, response, body){
         if (!err && response.statusCode == 200) {
             var info = JSON.parse(body);
-            res.send(info);
+            var displaySearches = [];
+            var displaySearch = {};
+            for(var i = 0; i < info.items.length; i++){
+                displaySearch = {};
+                displaySearch.url = info.items[i].link;
+                displaySearch.snippet = info.items[i].snippet;
+                displaySearch.thumbnail = info.items[i].image.thumbnailLink;
+                displaySearch.context = info.items[i].image.contextLink;
+                displaySearches.push(displaySearch);
+            }
+            res.send(displaySearches);
         }
     }
+}
+
+function createHistory(req){
+    var userRequest = {};
+    userRequest.search = req.params.query;
+    userRequest.time = new Date();
+
+    Search.create(userRequest, function(err, search){
+        if(err){ console.error(err) }
+    });
 }
